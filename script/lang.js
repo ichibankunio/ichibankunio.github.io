de7238f65b02d8dcb79514070b0c1a7571ee0b22
ichibankunio <kunio.ichiban@gmail.com>
2022-06-03 10:21:41 +0900

    fixed

---
+++ script/lang.js
@@ -0,0 +1,32 @@
+
+function getLanguage() {
+    const threeToTwo = {
+        'eng': 'en',
+        'jpn': 'ja',
+    };
+    const lang = navigator.language.match(/^[a-zA-Z]{2,3}/)[0];
+    if (lang.length === 3) {
+        lang = threeToTwo[lang];
+    }
+    if (new Set(['en', 'ja']).has(lang)) {
+        return lang;
+    }
+    return 'en';
+}
+
+function updateLanguage(lang) {
+    for (const e of document.querySelectorAll('*[lang]:not(html)')) {
+        if (e.lang === lang) {
+            e.style = "display: block;";
+        } else {
+            e.style = "display: none;";
+
+        }
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    let lang = getLanguage();
+    updateLanguage(lang);
+    
+});
