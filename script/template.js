3bbb65e6118420d91fe15070e5c0fbb12e0f5960
ichibankunio <kunio.ichiban@gmail.com>
2023-01-12 01:01:32 +0900

    made header common in every page

--- script/template.js
+++ script/template.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     // var header = document.getElementById("header");
+
     var header = document.getElementsByTagName("header");
 	header[0].insertAdjacentHTML("afterbegin", "<ul class=\"header-list\"><li class=\"header-list-item\"><a lang=\"en\" href=\"https://ichibankunio.github.io/index.html#\">Home</a><a lang=\"ja\" href=\"https://ichibankunio.github.io/index.html#\">ホーム</a></li><li class=\"header-list-item\"><a lang=\"en\" href=\"https://ichibankunio.github.io/index.html#works\">Works</a><a lang=\"ja\" href=\"https://ichibankunio.github.io/index.html#works\">作ったゲーム</a></li><li class=\"header-list-item\"><a lang=\"en\" href=\"https://ichibankunio.github.io/index.html#news\">News</a><a lang=\"ja\" href=\"https://ichibankunio.github.io/index.html#news\">最新情報</a></li><li class=\"header-list-item\" id=\"selectItem\"><select id=\"langSelect\"><option value=\"en\">English</option><option value=\"ja\">日本語</option></select></li></ul>")
 
@@ -7,6 +8,27 @@ window.addEventListener('DOMContentLoaded', () => {
     updateLanguage(lang);
     
 });
+
+// <ul class="header-list">
+//     <li class="header-list-item">
+//         <a lang="en" href="https://ichibankunio.github.io/index.html#">Home</a>
+//         <a lang="ja" href="https://ichibankunio.github.io/index.html#">ホーム</a>
+//     </li>
+//     <li class="header-list-item">
+//         <a lang="en" href="https://ichibankunio.github.io/index.html#works">Works</a>
+//         <a lang="ja" href="https://ichibankunio.github.io/index.html#works">作ったゲーム</a>
+//     </li>
+//     <li class="header-list-item">
+//         <a lang="en" href="https://ichibankunio.github.io/index.html#news">News</a>
+//         <a lang="ja" href="https://ichibankunio.github.io/index.html#news">最新情報</a>
+//     </li>
+//     <li class="header-list-item" id="selectItem">
+//         <select id="langSelect">
+//             <option value="en">English</option>
+//             <option value="ja">日本語</option>
+//         </select>
+//     </li>
+// </ul>
 
 // function header() {
 // 	var header = document.getElementById("header");
